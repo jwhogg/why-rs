1a5cd3b0d7a187c5d52683691cdea6e50112f977
Joel Hogg <jhogg2@sheffield.ac.uk>
2025-12-04 14:53:53 +0000

    refactor project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,7 @@
-// // Declare all your main modules
-// // This tells Rust to look for `src/core/mod.rs` (or `src/core.rs`)
-// pub mod core;
-// pub mod io;
-// pub mod stats;
-// pub mod discovery;
-// pub mod error;
-//
-// // You can also "re-export" the most important items
-// // so users can access them easily.
-// // e.g., `use causal_rs::CausalGraph` instead of `use causal_rs::core::CausalGraph`
-// pub use core::CausalGraph;
-// pub use io::load_csv;
-// pub use discovery::pc_algorithm; // Assuming the fn is `pc_algorithm`
-// pub use error::CausalError;
+pub mod dag;
+pub mod fcm;
+#[macro_use]
+mod macros;
+
+pub use crate::dag::DAG;
+pub use crate::fcm::FCM;
--- src/stats/fisher_z.rs
+++
@@ -1,4 +0,0 @@
-pub struct FishersZ;
-
-// impl ConditionalIndependenceTest for FishersZ {
-// }
