d04d1ebaea9211bbeda867c163957be9ef0291d1
Joel Hogg <jhogg2@sheffield.ac.uk>
2025-12-05 10:30:24 +0000

    example using a functional equation

--- examples/fcm_test.rs
+++ examples/fcm_test.rs
@@ -1,5 +1,5 @@
 use why_rs::fcm::FCM;
-use why_rs::dag::DAG;
+use why_rs::dag::{Variable, DAG};
 use why_rs::dag;
 use why_rs::fcm::multivariate_linear;
 use rand::Rng;
@@ -10,8 +10,19 @@ fn main() {
             "B" => "C"
         );
 
-    let fcm = FCM::from_dag(dag);
+    let mut fcm = FCM::from_dag(dag);
 
     let noise_c = rand::thread_rng().gen_range(0..5);
-    fcm.rule("C", multivariate_linear(vec![0.2 as u32, 0.5 as u32], 2, noise_c));
+    fcm = fcm.rule("C", multivariate_linear(vec![0.2 as u32, 0.5 as u32], 2, noise_c));
+
+    let m = fcm.get_mechanism(&Variable::from("C")).unwrap();
+    let inputs = vec![10, 20]; // Value of A=10, Value of B=20
+
+    // 3. Execute the closure
+    let result = m(&inputs);
+    println!("Input: A=10, B=20");
+    println!("Calculation: (10 * 0.2) + (20 * 0.5) + bias(2) + noise({})", noise_c);
+    println!("Output: {}", result);
+
+
 }
